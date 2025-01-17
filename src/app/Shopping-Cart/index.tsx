8e148384f544a57ab9705f3b82bfe65becf5f138
pmz10 <welcome_to_pmz@hotmail.com>
2025-01-16 22:31:59 -0600

    Add CartCounter component and update Sidebar with menu items

---
+++ src/app/Shopping-Cart/index.tsx
@@ -0,0 +1,2 @@
+
+export { CartCounter } from './Components/CartCounter';
--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,8 +1,19 @@
+import { CartCounter } from "@/app/Shopping-Cart";
+
+export const metadata = {
+  title: 'Shopping Cart',
+  description: 'Un simple contador'
+  }
 
 export default function CounterPage() {
+
+
+
   return (
-    <div>
-      <h1>Counter Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en carrito</span>
+
+      <CartCounter value={10} />
     </div>
   );
 }
