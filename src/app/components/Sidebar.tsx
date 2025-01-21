4b8cb5eb3c3adedf9e4382d6dd01ae7d13f7a83e
pmz10 <welcome_to_pmz@hotmail.com>
2025-01-21 16:51:00 -0600

    Add Pokémon dashboard and related components

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import { IoBrowsersOutline, IoCalculator, IoLogoReact } from 'react-icons/io5'
+import { IoBrowsersOutline, IoCalculator, IoFootball, IoLogoReact } from 'react-icons/io5'
 import { SidebarMenuitem } from './SidebarMenuitem'
 
 const menuItems = [
@@ -16,11 +16,17 @@ const menuItems = [
         title: 'Counter',
         subTitle: 'Contador Client Side'
     },
+    {
+        path: '/dashboard/pokemons',
+        icon: <IoFootball size={40} />,
+        title: 'Pokemons',
+        subTitle: 'Generación Estática'
+    },
 ]
 
 export const Sidebar = () => {
     return (
-        <div id="menu" style={{ width: '400px' }} className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 h-screen overflow-y-scroll">
+        <div id="menu" style={{ width: '400px' }} className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 overflow-y-scroll">
             <div id="logo" className="my-4 px-6">
                 <h1 className="flex items-center text-lg md:text-2xl font-bold text-white">
                     <IoLogoReact className="mr-2" />
---
+++ src/app/dashboard/pokemons/page.tsx
@@ -0,0 +1,40 @@
+import { PokemonGrid, PokemonsReponse, SimplePokemon } from "@/app/pokemons";
+import Image from 'next/image';
+
+const getPokemons = async (limit = 20, offset = 0): Promise<SimplePokemon[]> => {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data: PokemonsReponse = await response.json();
+        const pokemons = data.results.map(pokemon => ({
+            id: pokemon.url.split('/')[6],
+            name: pokemon.name,
+            url: pokemon.url
+        }));
+        return pokemons;
+    } catch (error) {
+        console.error('Error fetching pokemons:', error);
+        return [];
+    }
+}
+
+export default async function PokemonsPage() {
+    const pokemons = await getPokemons(151);
+
+    if (!pokemons) {
+        return (
+            <div>
+                <p>Error fetching pokemons. Please try again later.</p>
+            </div>
+        );
+    }
+
+    return (
+        <div className="flex flex-col">
+            <span className="text-5xl my-2">Listado de Pokémons <small>estático</small></span>
+           <PokemonGrid pokemons={pokemons} />
+        </div>
+    );
+}
